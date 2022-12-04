d38260e561923335e991b5a83720fc3a9dfe0e1e
Mariya Demy <Mariya.Demi373@gmail.com>
2022-12-04 16:35:00 +0300

    add: complete task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,19 @@
+const path = require('path');
+const {mkdir, copyFile, readdir, rm} = require('fs/promises');
 
+let copyFolderPath = path.join(__dirname, 'files-copy');
+let srcFolderPath = path.join(__dirname, 'files');
+
+async function copyDir(){
+    try {
+        await rm(copyFolderPath, { recursive: true, force: true}); //remove folder first, if there is no one to delete ignore the command (via force: true)
+        await mkdir(copyFolderPath, { recursive: true });
+        const files = await readdir(srcFolderPath);
+        for (const file of files){
+            await copyFile(srcFolderPath + "\\" + file, copyFolderPath + "\\" + file);
+        }
+    } catch (err) {
+        console.error(err.message);
+    }
+}
+copyDir();
