2a8a3dc99e4cc26c7c06354d23ec44438e527397
Mariya Demy <Mariya.Demi373@gmail.com>
2023-02-05 20:15:26 +0300

    add: complete task 5

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const {readdir, readFile, appendFile, rm} = require('fs/promises');
+
+const distFolderPath = path.join(__dirname, 'project-dist');
+const stylesFolderPath = path.join(__dirname, 'styles');
+const bundleFilePath = distFolderPath + "\\bundle.css";
+
+async function mergeStyles(){
+    try {
+    await rm(bundleFilePath, {force: true});
+    const files = await readdir(stylesFolderPath, {withFileTypes: true});
+    for (const file of files){
+        if(file.isFile() && /\.css$/.test(file.name)){
+            const fileContents = await readFile(`${stylesFolderPath}\\${file.name}`, { encoding: 'utf8' });
+            await appendFile(bundleFilePath, fileContents);
+        }
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+mergeStyles();
