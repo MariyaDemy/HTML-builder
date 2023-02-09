fc6d8d3d155f6cc9a7e0d213d5f8a8e57b952edf
Mariya Demy <Mariya.Demi373@gmail.com>
2023-02-09 15:57:27 +0300

    refactor: copy files and subfolders

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,16 +4,22 @@ const {mkdir, copyFile, readdir, rm} = require('fs/promises');
 let copyFolderPath = path.join(__dirname, 'files-copy');
 let srcFolderPath = path.join(__dirname, 'files');
 
-async function copyDir(){
+async function copyDir(sourceFolder, targetFolder){
     try {
-        await rm(copyFolderPath, { recursive: true, force: true}); //remove folder first, if there is no one to delete ignore the command (via force: true)
-        await mkdir(copyFolderPath, { recursive: true });
-        const files = await readdir(srcFolderPath);
-        for (const file of files){
-            await copyFile(srcFolderPath + "\\" + file, copyFolderPath + "\\" + file);
+        //remove folder first, if there is no one to delete ignore the command (via force: true)
+        await rm(targetFolder, { recursive: true, force: true});
+        await mkdir(targetFolder, { recursive: true });
+        const contents = await readdir(sourceFolder, {withFileTypes: true});
+          for (const content of contents){
+          if(content.isFile()){
+              await copyFile(sourceFolder + `\\${content.name}`, targetFolder + `\\${content.name}`);
+          } else {
+              copyDir(sourceFolder + `\\${content.name}`, targetFolder + `\\${content.name}`)
+          }
         }
     } catch (err) {
         console.error(err.message);
     }
 }
-copyDir();
+
+copyDir(srcFolderPath, copyFolderPath);
